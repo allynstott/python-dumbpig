c755a535a502f47c36f1a7f79d4ed3407f1bd72f
Allyn Stott <astott@palantir.com>
2015-06-06 23:35:18 -0700

    Fixed setup

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     version="0.0.2",
     description="Python library for Dumbpig",
     author="Ryan Hays, Allyn Stott",
-    author_email="ryan@oculussec.com, allynstott@gmail.com"
+    author_email="ryan@oculussec.com, allynstott@gmail.com",
     packages=["dumbpig"],
     package_dir={"dumbpig": "."},
     license="GNU General Public License (GPL) 3.0")
